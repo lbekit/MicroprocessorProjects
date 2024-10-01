215cbb4db1af1c74579710cb39007696fefcc3f5
lbekit <lbekit@usf.edu>
2024-10-01 06:52:31 -0400

    creating initial Microprocessors Repository

---
+++ Debug/board/peripherals.d
@@ -0,0 +1,33 @@
+board/peripherals.o board/peripherals.d: ../board/peripherals.c \
+ ../board/peripherals.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_common.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\device/fsl_device_registers.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\device/LPC55S69_cm33_core0.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\CMSIS/core_cm33.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\CMSIS/cmsis_version.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\CMSIS/cmsis_compiler.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\CMSIS/cmsis_gcc.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\CMSIS/mpu_armv8.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\device/system_LPC55S69_cm33_core0.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\device/LPC55S69_cm33_core0_features.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_common_arm.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_clock.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_common.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_reset.h \
+ C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_pint.h
+../board/peripherals.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_common.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\device/fsl_device_registers.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\device/LPC55S69_cm33_core0.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\CMSIS/core_cm33.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\CMSIS/cmsis_version.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\CMSIS/cmsis_compiler.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\CMSIS/cmsis_gcc.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\CMSIS/mpu_armv8.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\device/system_LPC55S69_cm33_core0.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\device/LPC55S69_cm33_core0_features.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_common_arm.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_clock.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_common.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_reset.h:
+C:\Users\bekit\Documents\MCUXpressoIDE_11.10.0_3148\workspace\LPC55S69_Project\drivers/fsl_pint.h:
